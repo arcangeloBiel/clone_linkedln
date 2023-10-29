d0f59a449e5c2048effb7cf0b1d380b3a27d4b91
joao ribeiro <joao.ribeiro@havan.com.br>
2023-10-29 08:57:30 -0300

    adiciona crashlytics

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -96,5 +96,6 @@ dependencies {
     //firebase
     implementation(platform("com.google.firebase:firebase-bom:32.4.0"))
     implementation("com.google.firebase:firebase-analytics-ktx")
+    implementation ("com.google.firebase:firebase-crashlytics-ktx")
 
 }
